2019622615cdf154bda5d40d30852d9d0cfd498d
Arafet Ben Kilani <arafet.benkilani@supcom.tn>
2023-08-30 23:56:16 +0100

    added firebase

--- src/routes/login/login.component.jsx
+++ src/routes/login/login.component.jsx
@@ -1,8 +1,16 @@
+import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils"
 
 const Login = () => {
+
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup()
+    console.log(response.user)
+  }
+
   return (
     <div>
         <h1>This is signin page</h1>
+        <button type="submit" onClick={logGoogleUser}>Sign IN</button>
     </div>
   )
 }
