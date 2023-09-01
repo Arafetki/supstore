0b54e78a55fa718093ad44c4b9c6ccf234fde731
Arafet Ben Kilani <arafet.benkilani@supcom.tn>
2023-09-01 04:10:01 +0100

    added firestore

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,6 @@
 import {initializeApp} from 'firebase/app'
 import {GoogleAuthProvider, getAuth,signInWithPopup} from 'firebase/auth'
+import {doc,getDoc,setDoc,getFirestore} from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyCcdxXNJy1hOIufdSFT0jf4K1nHCpSM_LU",
@@ -8,7 +9,7 @@ const firebaseConfig = {
     storageBucket: "supc-clothing.appspot.com",
     messagingSenderId: "123103610579",
     appId: "1:123103610579:web:a69b53a27ce17e792fb626"
-  };
+};
 
 const fireApp = initializeApp(firebaseConfig);
 
@@ -21,3 +22,28 @@ provider.setCustomParameters({
 export const auth = getAuth(fireApp)
 
 export const signInWithGooglePopup = () => signInWithPopup(auth,provider)
+
+export const fireDB = getFirestore(fireApp)
+
+export const createUserFromAuth = async (userAuth) => {
+  const userDocRef = doc(fireDB,'users',userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef)
+
+  if (!userSnapshot.exists()){
+    const {displayName,email} = userAuth
+    const created = new Date()
+
+    try {
+      await setDoc(userDocRef,{
+        displayName,
+        email,
+        created
+      })
+    }catch(err){
+      console.error("an error occured",err.message);
+    }
+  }
+
+  return userDocRef
+} 
